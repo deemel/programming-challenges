d1d58fef7d5d8bb567cf50d6b07ceaf730b120b1
deemel <40222382+deemel@users.noreply.github.com>
2024-10-16 11:27:37 -0400

    Update rps.py

--- rock-paper-scissors/rps.py
+++ rock-paper-scissors/rps.py
@@ -1,37 +1,33 @@
+#!/usr/bin/env python3
+ 
 from enum import IntEnum
 import random
-
-OUTCOMES = [[0, 2, 1],
-            [1, 0, 2],
-            [2, 1, 0]]
-RESULTS = ['draw', 'player 1 wins', 'player 2 wins']
-
-
+ 
+class RPS():
+    OUTCOMES = [[0, 2, 1],
+                [1, 0, 2],
+                [2, 1, 0]]
+    RESULTS = ["draw", "hand 1 wins", "hand 2 wins"]
+ 
 class Hand(IntEnum):
     ROCK = 0
     PAPER = 1
     SCISSOR = 2
-
-    def compete(self, opponent: 'Hand') -> int:
-        return OUTCOMES[self][opponent]
-
-    def __str__(self):
-        return self.name
-
-
-def match(item1, item2):
-    print(f'{item1.name} vs {item2.name}: {RESULTS[item1.compete(item2)]}')
-
-
-def generateitems(n):
+ 
+    def getOutcome(self, opponent: "Hand") -> int:
+        return RPS.OUTCOMES[self][opponent]
+ 
+    def compete(self, opponent: "Hand") -> str:
+        return f"{self.name:>{len(self.SCISSOR.name)}} vs {opponent.name:>{len(self.SCISSOR.name)}}: {RPS.RESULTS[self.getOutcome(opponent)]}"
+ 
+def generateHands(n):
     # https://www.geeksforgeeks.org/python-which-is-faster-to-initialize-lists/
     # return zip([Hand(random.randint(Hand.ROCK, Hand.SCISSOR)) for _ in range(n)], [Hand(random.randint(Hand.ROCK, Hand.SCISSOR)) for _ in range(n)])
 
     # https://stackoverflow.com/questions/231767/what-does-the-yield-keyword-do
     for _ in range(n):
         yield Hand(random.randint(Hand.ROCK, Hand.SCISSOR)), Hand(random.randint(Hand.ROCK, Hand.SCISSOR))
-
-
-if __name__ == '__main__':
-    for i1, i2 in generateitems(20):
-        match(i1, i2)
+ 
+if __name__ == "__main__":
+    for hand1, hand2 in generateHands(20):
+        print(hand1.compete(hand2))
